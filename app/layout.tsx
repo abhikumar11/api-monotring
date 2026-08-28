861b30bc225a3a94195f33993be09596444285e1
abhikumar11 <abhishekkumar12verma@gmail.com>
2026-08-28 15:08:29 +0530

    initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
+import { ToastContainer } from "react-toastify";
 import "./globals.css";
 
-
-interface LayoutProps{
-  children: React.ReactNode;
+interface LayoutProps {
+     children: React.ReactNode;
 }
 export default function RootLayout({ children }: LayoutProps) {
-  return (
-    <html
-      lang="en"
-      className={"h-full antialiased"}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+     return (
+          <html lang="en" className={"h-full antialiased"}>
+               <body className="min-h-full flex flex-col">
+                    <ToastContainer position="top-right" autoClose={5000} />
+
+                    {children}
+               </body>
+          </html>
+     );
 }
