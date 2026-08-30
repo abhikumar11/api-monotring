e7915d645a7610b8d3261395e8b45d390d2dd899
abhikumar11 <abhishekkumar12verma@gmail.com>
2026-08-30 15:38:23 +0530

    auth functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ToastContainer } from "react-toastify";
 import "./globals.css";
+import AppProviders from "@/providers/appProviders";
 
 interface LayoutProps {
      children: React.ReactNode;
@@ -8,9 +9,14 @@ export default function RootLayout({ children }: LayoutProps) {
      return (
           <html lang="en" className={"h-full antialiased"}>
                <body className="min-h-full flex flex-col">
-                    <ToastContainer position="top-right" autoClose={5000} />
+                    <AppProviders>
+                         <ToastContainer
+                              position="top-right"
+                              autoClose={5000}
+                         />
 
-                    {children}
+                         {children}
+                    </AppProviders>
                </body>
           </html>
      );
